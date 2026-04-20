c6f20416eb9e6e0481c0df9db7de5b69674d8f19
yashashvibisenbatch2024-cmd <yashashvi.bisen.batch2024@sitnagpur.siu.edu.in>
2026-04-20 15:24:44 +0530

    Create StudentMain.java

---
+++ StudentMain.java
@@ -0,0 +1,21 @@
+public class StudentsMain {
+
+    public static void main(String[] args) {
+
+        Students s1 = new Students();
+
+        // s1.setRollNo(100);
+        // s1.setStudName("Abc");
+        // s1.setStudBranch("CSE");
+
+        System.out.println(s1.getRollNo());
+        System.out.println(s1.getStudName());
+        System.out.println(s1.getStudBranch());
+
+        Students s2 = new Students(101, "Xyz", "CSE");
+
+        System.out.println(s2.getRollNo());
+        System.out.println(s2.getStudName());
+        System.out.println(s2.getStudBranch());
+    }
+}
