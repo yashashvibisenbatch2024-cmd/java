5a88bfc98863bcdee1296e4c94bd9fd47394976f
yashashvibisenbatch2024-cmd <yashashvi.bisen.batch2024@sitnagpur.siu.edu.in>
2026-04-20 15:16:55 +0530

    Create AdditionMain.java

---
+++ AdditionMain.java
@@ -0,0 +1,13 @@
+public class AdditionMain {
+
+    public static void main(String[] args) {
+
+        Addition obj = new Addition();
+
+        obj.add();
+        obj.add(10);
+        obj.add(10, 20);
+
+    }
+
+}
